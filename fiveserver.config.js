8d1005f8e7b484ec50ca5c550fcd0f25d7b7e542
paraisogay <151970089+paraisogay@users.noreply.github.com>
2023-11-24 22:31:43 +0100

    Add files via upload

---
+++ fiveserver.config.js
@@ -0,0 +1,21 @@
+
+//fiverserver.config.js
+
+ 
+module.exports = {
+
+
+
+
+highlight: true, //enable highlight feature
+injectBody: true, //enable instant update
+remoteLogs:true, //enable remoteLogs
+remoteLogs:"yellow", //enable remoteLogs and use color
+injectCss:false,    //disable injecting CSS
+navigate: true, //enable autonavigation
+
+
+
+
+
+};
